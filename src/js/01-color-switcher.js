e12ffdbafa26c68fae2670f6df14364f521bc1d6
Sergii Vlasenko <98455988+nepiushiu1@users.noreply.github.com>
2022-09-05 21:35:42 +0300

    1 comit
    1 comit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const background = document.querySelector('body');
+let intervalId = null;
+
+start.addEventListener('click', processStart);
+
+function processStart() {
+  intervalId = setInterval(() => {
+    background.style.backgroundColor = getRandomHexColor();
+    console.log('проверка');
+  }, 1000);
+  start.disabled = true;
+}
+
+stop.addEventListener('click', processStop);
+
+function processStop() {
+  clearInterval(intervalId);
+  start.disabled = false;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
